a58dc37e5457777264e8dbd59fe25d5e843c0677
rblyimer <101184162+rblyimer@users.noreply.github.com>
2022-07-25 16:22:41 -0400

    css and js for home

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+//mobile toggle for nav
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+//change the nav bar background color when scrolled
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
