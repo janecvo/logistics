f9f3dd6f9ab7519fc8431b1d5e30c8bccc68f654
Cameron <bmscameronb072a@yahoo.com>
2022-07-27 08:27:51 -0400

    contact page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,70 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAzadFkol2qPJm1K0F-H8waGKJ_2cSVzRw",
+  authDomain: "freight-liner-data.firebaseapp.com",
+  projectId: "freight-liner-data",
+  storageBucket: "freight-liner-data.appspot.com",
+  messagingSenderId: "90778119169",
+  appId: "1:90778119169:web:cece8d6deb836c09bca400"
+};
+//Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var firestore = firebase.firestore();
+
+//Variable to access database collection
+const db = firestore.collection("formData");
+
+//Get Submit Form
+let submitButton = document.getElementById("submit");
+
+//Create Event Listener To Allow Form Submission
+submitButton.addEventListener("click", (e) => {
+  //Prevent Default Form Submission Behavior
+  e.preventDefault();
+
+  //Get Form Values
+  let fName = document.getElementById("fName").value;
+  let email = document.getElementById("email").value;
+  let subject = document.getElementById("subject").value;
+  let message = document.getElementById("message").value;
+
+  firestore
+    .collection("formData")
+    .get()
+    .then((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        const fn = doc.data().fName;
+        if (fName === fn) {
+          console.log("Already Exists");
+        }
+
+        // console.log("data", doc.data().fname);
+      });
+    });
+  //Save Form Data To Firebase
+  db.doc()
+    .set({
+      fName: fName,
+      email: email,
+      subject: subject,
+      message: message,
+    })
+    .then(() => { })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  //alert
+  alert("Your Form Has Been Submitted Successfully");
+
+  //clear form after submission
+  function clearForm() {
+    document.getElementById("clearFrom").reset();
+  }
+  clearForm()
+});
+
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+// Form information
