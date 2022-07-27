3f3d3876a37b9cb3401aeac4ca53cb367c64fe33
rblyimer <101184162+rblyimer@users.noreply.github.com>
2022-07-27 08:43:06 -0400

    about page

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,27 @@ window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+//partner logos carousel
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
