e29ce8833dbc20de98a83394d323aa09fde33f10
rblyimer <101184162+rblyimer@users.noreply.github.com>
2022-07-29 10:56:18 -0400

    about page done

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,23 +1,4 @@
-//partner logos carousel
-$(document).ready(function(){
-    $('.customer-logos').slick({
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        arrows: false,
-        dots: false,
-        pauseOnHover: false,
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 4
-            }
-        }, {
-            breakpoint: 520,
-            settings: {
-                slidesToShow: 3
-            }
-        }]
-    });
-});
+//mobile toggle for nav
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+  }
--- assets/js/home.js
+++ assets/js/home.js
@@ -1,5 +1,10 @@
 //change the nav bar background color when scrolled
 window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
+    header.classList.toggle("sticky", window.scrollY > 10);
 })
+
+//mobile toggle for nav
+function toggleMobileMenu(menu) {
+  menu.classList.toggle('open');
+}
