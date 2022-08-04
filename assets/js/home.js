dd690e5d3c2d1b21c2910362621336bd549842a6
rblyimer <101184162+rblyimer@users.noreply.github.com>
2022-08-04 16:58:03 -0400

    homepage edits

--- assets/js/home.js
+++ assets/js/home.js
@@ -8,3 +8,15 @@ window.addEventListener("scroll", function(){
 function toggleMobileMenu(menu) {
   menu.classList.toggle('open');
 }
+
+$(".custom-carousel").owlCarousel({
+  autoWidth: true,
+  loop: true
+});
+$(document).ready(function () {
+  $(".custom-carousel .item").click(function () {
+    $(".custom-carousel .item").not($(this)).removeClass("active");
+    $(this).toggleClass("active");
+  });
+});
+
